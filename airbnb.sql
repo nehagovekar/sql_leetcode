0e9918821ded93ac267327e425ba3de764870432
nehagovekar <nehagovekar2198@gmail.com>
2025-05-04 22:29:52 -0700

    easy but tricky

---
+++ airbnb.sql
@@ -0,0 +1,28 @@
+/*
+33. What is the top renting property?
+146
+65%
+Find the top rented property(s) id from the following table. Top rented can be thought of as number of nights rented. Please consider ties and column names. Return results sorted in asc order.
+
+window
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+property_id
+7
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+airbnb_fct_rentals	
+user_id	bigint
+property_id	bigint
+num_of_nights	bigint
+price_per_night	double
+purchased_date	date
+
+*/
