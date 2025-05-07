1e6d2e2e40fa58c7023b030ae3eef5677ace1e6a
nehagovekar <nehagovekar2198@gmail.com>
2025-05-06 19:44:12 -0700

    hard

--- apple_hard.sql
+++ apple_hard.sql
@@ -37,3 +37,22 @@ payment_method	varchar
 price	double
 device	varchar
 rating	double*/
+WITH filtered_data AS 
+  (SELECT att.payment_method, (CASE WHEN risk_score>=0.80 THEN 1 ELSE 0 END) AS pred_label, true_label FROM appstore_transaction_risk atr 
+  JOIN appstore_transactions att 
+ON atr.transaction_id= att.transaction_id
+WHERE att.payment_method IS NOT NULL),
+ conf_mat AS
+(
+  SELECT payment_method,
+  COUNT(CASE WHEN (pred_label=1 AND true_label=1) THEN 1 ELSE null END) AS TP,
+  COUNT(CASE WHEN (pred_label=0 AND true_label=0) THEN 1 ELSE null END ) AS TN,
+  COUNT(CASE WHEN (pred_label=1 AND true_label=0) THEN 1 ELSE null END ) AS FP,
+  COUNT(CASE WHEN (pred_label=0 AND true_label=1) THEN 1 ELSE null END ) AS FN,
+  FROM filtered_data
+  GROUP BY payment_method
+
+)
+  SELECT payment_method, ROUND(COALESCE((TP/(TP+FP)),0),2) as precision_score, ROUND(COALESCE((TP/(TP+FN)),0),2) as recall_score
+FROM conf_mat
+  ORDER BY payment_method ASC;
