f11aef1ed0f1da50a95a3c21a5b5606751b1ecbd
nehagovekar <nehagovekar2198@gmail.com>
2025-05-06 19:48:12 -0700

    easy

---
+++ meta_Easy2.sql
@@ -0,0 +1,27 @@
+/*
+39. What is the percentage of users who are banned?
+146
+65%
+Find the percent of users who are permanently banned. Round your answer to 2 decimals.
+
+metric
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+permanent_banned_prcnt
+10
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+banned_users	
+user_id	bigint
+banned_reason	varchar
+banned_on	date
+is_temporary	bigint
+
+*/
