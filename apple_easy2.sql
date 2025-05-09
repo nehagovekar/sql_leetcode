c79e63b5f7fecde79f4d8bab3c97b920ede5d7af
nehagovekar <nehagovekar2198@gmail.com>
2025-05-08 18:04:29 -0700

    apple easy

---
+++ apple_easy2.sql
@@ -0,0 +1,42 @@
+/*
+50. Apple user with no App Store activity
+146
+65%
+Which users who joined between Jan 1, 2022 and Jan 8, 2022 never made a single purchase on App Store between ? Output the email and joined date of users sorted by email ascending
+
+join
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+email	joined_dt
+AAnderson4@yahoo.com	2022-01-07T00:00:00
+CHernandez3@gmail.com	2022-01-05T00:00:00
+RBailey6@gmail.com	2022-01-08T00:00:00
+RCox2@gmail.com	2022-01-01T00:00:00
+SRodriguez9@gmail.com	2022-01-05T00:00:00
+VGardner5@hotmail.com	2022-01-05T00:00:00
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+appstore_transactions	
+transaction_id	bigint
+user_id	bigint
+app_name	varchar
+transaction_dt	timestamp
+payment_method	varchar
+price	double
+device	varchar
+rating	double
+apple_users	
+user_id	bigint
+first_name	varchar
+last_name	varchar
+email	varchar
+country	varchar
+joined_dt	date
+*/
