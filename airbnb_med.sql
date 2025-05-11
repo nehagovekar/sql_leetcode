e306c1cd20245e25b176759dbec7b1d68f21aae5
nehagovekar <nehagovekar2198@gmail.com>
2025-05-11 08:51:10 -0700

    med

---
+++ airbnb_med.sql
@@ -0,0 +1,35 @@
+/*
+59. Whos the most frequent renter?
+146
+65%
+Find the most frequent renter at AirBnB. Allow for ties, return all tied for first if ties exist.
+
+window
+string
+join
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+renter_name	total_rentals
+Michael Shah	10
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+airbnb_fct_rentals	
+user_id	bigint
+property_id	bigint
+num_of_nights	bigint
+price_per_night	double
+purchased_date	date
+airbnb_dim_users	
+user_id	bigint
+first_name	varchar
+last_name	varchar
+has_rented	bigint
+joined_on_date	date
+*/
