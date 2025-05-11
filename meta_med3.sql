3c9cd378c50f19eb971dd803118131d546068eec
nehagovekar <nehagovekar2198@gmail.com>
2025-05-10 20:53:46 -0700

    med

---
+++ meta_med3.sql
@@ -0,0 +1,31 @@
+/*
+58. What is the Cost Per Click (CPC) per ad?
+146
+65%
+Find the total Ad Spend spend for each ad. Ad spend is a sumation of CPC, or Cost Per Click. This is what advertisers agree to pay for in campaign setup. Sort answer by ad spend desc and ad asc. Round all answers to 2 decimals.
+
+math
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+ad_name	ad_spend
+Charty Inc Trial	1.29
+Cosmetics Holiday Sale	1.28
+Disney+ Bundle	0.35
+Spotify Prem	0.3
+Nike Shoe Release	0.15
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+ads_actions	
+ad_exp_id	bigint
+ad_name	varchar
+cpc_rate	varchar
+clicked_ad	bigint
+
+*/
