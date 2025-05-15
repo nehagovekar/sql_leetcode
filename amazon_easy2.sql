c0e61d359cd2d5fcf459ae2c752776d14b0c4c73
nehagovekar <nehagovekar2198@gmail.com>
2025-05-14 17:52:39 -0700

    easy

---
+++ amazon_easy2.sql
@@ -0,0 +1,34 @@
+/*
+61. The most expensive product per category
+146
+65%
+For each product category, which product is the most expensive? Output the category, product name and price in the alphabetical order of category. Only return one product per category.
+
+rank
+filter
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+category	product_name	price
+book	Practical Statistics	16.99
+clothing	Pantagonia Insulated Jacket	109.5
+electronics	2021 Macbook 15 inch	1599.95
+game	PlayStation 5	400
+jewelry	14K Gold Ring	289.95
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+amazon_products	
+product_id	bigint
+product_name	varchar
+category	varchar
+price	double
+rating	bigint
+published_at	date
+
+*/
