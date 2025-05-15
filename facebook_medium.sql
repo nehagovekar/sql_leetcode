315218897f174ca0d8821ebca9e04a659992c2d0
nehagovekar <nehagovekar2198@gmail.com>
2025-05-14 18:16:08 -0700

    med

---
+++ facebook_medium.sql
@@ -0,0 +1,30 @@
+/*
+med
+62. Whats the content generation for Jan on Facebook?
+146
+65%
+You're on the content team at Meta and are asked to prepare a prettyifed report for leadership breaking down the counts of each post type for the month of January 2022.
+
+case
+datetime
+group by
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+mon_yr	posts	ads	video	photos	polls
+2022-Jan	0	1	0	2	0
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+fb_posts	
+post_id	bigint
+user_id	bigint
+creation_date	timestamp
+post_type	varchar
+
+*/
