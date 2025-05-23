240117398c12558ec4547454c5fa3c4615edc5f8
nehagovekar <nehagovekar2198@gmail.com>
2025-05-22 19:10:03 -0500

    apple lag

---
+++ apple_med4.sql
@@ -0,0 +1,30 @@
+/*
+69. Average days between apple sign-ups
+146
+65%
+What is the average days between apple account sign-ups? Round to 2 decimal places.
+
+join
+datetime
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+avg_days_between_signups
+0.61
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+apple_users	
+user_id	bigint
+first_name	varchar
+last_name	varchar
+email	varchar
+country	varchar
+joined_dt	date
+
+*/
