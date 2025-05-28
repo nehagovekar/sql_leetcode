b905e57f0bc0031cce6939442ed19b2b6bc0128f
nehagovekar <nehagovekar2198@gmail.com>
2025-05-27 20:47:01 -0500

    easy

---
+++ uber_med6.sql
@@ -0,0 +1,27 @@
+/*
+80. What refund type is biggest reason for refunds?
+146
+65%
+Find the ride refund rate of all Uber trips by refund reason. Round answer to 2 decimals and sort by refund rate desc and reason asc
+
+math
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+refund_reason	refund_type_rate
+Poor Route	36.36
+Unprofessional	36.36
+Fare Dispute	9.09
+Other	9.09
+Quality	9.09
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+uber_refunds	
+trip_id	bigint
+refund_reason	varchar
+*/
