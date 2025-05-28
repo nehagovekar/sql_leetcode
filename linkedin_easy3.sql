eddb1317a10da0bbb98e4b7f0225bd7bf354ab09
nehagovekar <nehagovekar2198@gmail.com>
2025-05-28 17:26:32 -0500

    easy

---
+++ linkedin_easy3.sql
@@ -0,0 +1,27 @@
+/*
+79. How many posts have been posted just once?
+146
+65%
+How many job posts on Linkedin have been posted just once? Return the total number of single posts.
+
+group by
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+num_unique_post
+20
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+linkedin_job_posts	
+event_id	bigint
+post_id	bigint
+post_date	date
+company_id	bigint
+company	varchar
+*/
