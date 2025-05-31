b64b367824e3d789a50470df1581c0a82dc2195a
nehagovekar <nehagovekar2198@gmail.com>
2025-05-30 22:08:58 -0500

    med

---
+++ meta_med5.sql
@@ -0,0 +1,26 @@
+/*
+85. What is the percentage of posts that received a like of comment?
+146
+65%
+Find the percentage of posts that receive a like or comment raction type. Round your answer to 2 decimials.
+
+math
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+like_commnt_rate
+56.41
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+fb_posts_actions	
+post_id	bigint
+user_id	bigint
+action_datetime	timestamp
+reaction_type	varchar
+*/
