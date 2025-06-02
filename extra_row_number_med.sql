b7d2b1781f56a3fa0043bcad364f089efd729cc5
nehagovekar <nehagovekar2198@gmail.com>
2025-06-01 20:32:24 -0500

    med

---
+++ extra_row_number_med.sql
@@ -0,0 +1,34 @@
+/*
+86. Im Seeing Double
+146
+65%
+You have a dataset that contains changes to records over time. You realized its causing issues with joins because of duplicate keys. Your goal is to deduplicate the dataset so that each user only has 1 record based on the most recent change data. All other records are considered stale and can be omitted from your query. Return the user_id, primary_device, and email sorted by user_id asc.
+
+window
+Hint #1
+Company Similarity:
+
+This problem simulates interviews conducted at the following companies.
+
+Expected Output:
+
+Your answer should match this structure exactly. Keep in mind SQL sorting, grouping, & rounding when answering.
+user_id	primary_device	email
+1	Mobile - Android	xfoley@example.net
+2	Desktop	heatherwang@example.com
+3	Mobile - IOS	ryanjeffrey@example.net
+4	Mobile - Android	conradmichael@example.org
+5	Mobile - IOS	luis31@example.com
+6	Mobile - IOS	ereid@example.net
+7	Desktop	laura60@example.org
+8	Mobile - Android	joseph61@example.org
+Source Tables:
+
+Visualize the table definitions prior to formalizing your solution. Click the icon to see into the table's data.
+duplicated_data	
+user_id	varchar
+primary_device	varchar
+email	varchar
+last_updated_on	date
+
+*/
