5716f7dc8909b75c9d56bfcbf6b40c3e18243a70
nehagovekar <nehagovekar2198@gmail.com>
2025-06-20 09:53:45 -0500

    easy

---
+++ iq_4.sql
@@ -0,0 +1,29 @@
+/*
+Completed Shipments
+0:05:27
+
+28
+Question
+Solution
+My submissions
+User submissions
+Let’s say that you’re a data scientist working on a distribution team at Amazon.
+
+Create a report displaying which shipments were delivered to customers during their membership period.
+
+If the transaction is shipped during the membership period, column is_member should have the value Y. If not, the column should have the value N.
+
+Hint: You may assume that customer_id is unique and that shipments are made only to registered customers.
+
+Example:
+
+Input:
+
+customers table
+
+Column	Type
+customer_id	INTEGER
+membership_start_date	DATETIME
+membership_end_date	DATETIME
+
+*/
