1cf7b09e75b5942e19070ca866c11768745264ea
nehagovekar <nehagovekar2198@gmail.com>
2025-07-14 14:45:49 -0700

    med

--- leetcode_med.sql
+++ leetcode_med.sql
@@ -97,3 +97,10 @@ Output:
 | 4         | 2018-05-21 | 0              |
 +-----------+------------+----------------+
 */
+
+SELECT u.user_id as buyer_id, u.join_date, COALESCE(COUNT(o.order_id),0) as orders_in_2019
+FROM Users u
+LEFT JOIN
+Orders o
+ON u.user_id = o.buyer_id AND  year(o.order_date)='2019' 
+GROUP BY u.user_id, u.join_date;
